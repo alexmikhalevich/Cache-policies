320e0d02fd70a8ee733228c3e7c7990ce9423809
Alexander Mikhalevich <alex.mikhalevich@gmail.com>
2017-04-19 23:21:20 +0300

    LRU policy implementation

---
+++ src/com/cache/CLRUCache.java
@@ -0,0 +1,24 @@
+package com.cache;
+
+public class CLRUCache extends CAbstractCache {
+    public CLRUCache(int cache_size) {
+        super(cache_size);
+    }
+
+    public void put(Object key, Object val) {
+        if (_list.size() == _cache_size) {
+            prune();
+        }
+        _list.addFirst(key);
+        _map.put(key, val);
+    }
+
+    public Object get(Object key) {
+        boolean res = _list.remove(key);
+        if (res) {
+            _list.addFirst(key);
+            return _map.get(key);
+        }
+        return null;
+    }
+}
