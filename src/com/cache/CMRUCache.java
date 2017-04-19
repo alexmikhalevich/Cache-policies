d5e05623242bb8ded107d7e923485f82d9b95c50
Alexander Mikhalevich <alex.mikhalevich@gmail.com>
2017-04-19 23:20:37 +0300

    MRU policy implementation

---
+++ src/com/cache/CMRUCache.java
@@ -0,0 +1,26 @@
+package com.cache;
+
+public class CMRUCache extends CAbstractCache {
+    public CMRUCache(int cache_size) {
+        super(cache_size);
+    }
+
+    @Override
+    public void put(Object key, Object val) {
+        if (_list.size() == _cache_size) {
+            prune();
+        }
+        _list.addLast(key);
+        _map.put(key, val);
+    }
+
+    @Override
+    public Object get(Object key) {
+        boolean res = _list.remove(key);
+        if (res) {
+            _list.addLast(key);
+            return _map.get(key);
+        }
+        return null;
+    }
+}
